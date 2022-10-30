04ee7b71101fa07d83fd677077c8d68688a3792b
patrick mugambi <patrick@kali.kali>
2022-10-30 15:17:35 +0300

    material ui configurations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Button variant="text">Text</Button>
+<Button variant="contained" color='secondary' size='small'>Contained</Button>
+<Button disabled  variant="outlined">Outlined</Button>
     </div>
   );
 }
