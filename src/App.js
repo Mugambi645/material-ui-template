5c889dc70ded5da419422a43fa4414039f0b3360
patrick mugambi <patrick@kali.kali>
2022-10-31 18:49:36 +0300

    add,feed,post and navbar design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Feed from './components/Feed';
-import Rightbar from './components/Rightbar';
-import { Box, Container, Stack } from '@mui/material';
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-<Box>
-  {/*navbar*/}
-<Stack direction="row" spacing={2} justifyContent="space-between">
-<Sidebar/>
-<Feed/>
-<Rightbar/>
-</Stack>
-</Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar setMode={setMode} mode={mode}/>
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
